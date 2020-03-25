184fb6a214be9624618ee9ba7fb5050fae33e48b
Radoslaw Prusik <rkprusik@gmail.com>
2020-03-25 23:51:49 +0100

    [TASK III] Executed steps from task three with success.

--- L2 GLSL shaders Stud/shaders/specular.fs
+++ L2 GLSL shaders Stud/shaders/specular.fs
@@ -3,8 +3,40 @@
 // 
 varying vec3 N; // interpolowany wektor normalny powierzchni
 varying vec3 L[ 3 ]; // maksymalnie trzy intepolowane wektory œwiat³a
+//Zdefiniowanie w sta³ej const float specularExp wartoœci wyk³adnika odbicia lustrzanego (128).
+const float specularExp = 128;
 
 void main( void )
 {
    gl_FragColor = gl_Color; // skopiowanie g³ównego koloru (primary color)
+
+   //b. Zapisanie w zmiennej vec3 NN znormalizowanego wektora normalnego
+   vec3 NN = normalize(N);
+   //c. Zapisanie w zmiennej vec3 NL znormalizowanego wektora œwiat³a nr 0
+   vec3 NL = normalize(L[0]);
+   //d. Wyliczenie w zmiennej vec3 NH znormalizowanej sumy wektora NL oraz wektora (0.0, 0.0, 1.0)
+   vec3 NH = normalize(NL + (0.0, 0.0, 1.0));
+   //e. Obliczenie sk³adowej rozproszonej oœwietlenia (podobnie jak w 32.a i b) i zapisanie jej w zmiennej
+   //tymczasowej vec3 diffuse.
+    float intensity = max(0.0, dot(NN, NL));
+    vec3 diffuse = gl_FragColor.xyz * intensity;
+   
+   //f. Obliczenie sk³adowej odbicia lustrzanego vec3 specular, która jest równa wektorowi zerowemu,
+   //je¿eli intensywnoœæ intensity jest mniejsza lub równa 0.0. W przeciwnym wypadku, wyliczana jest na
+   //nowo intensywnoœæ, podobnie jak dla sk³adowej rozproszonej, jednak korzystaj¹c z wektora NH zamiast
+   //NL, a nastêpnie wyliczana jest sk³adowa odbicia lustrzanego jako intensywnoœæ do potêgi (pow())
+   //zdefiniowanego wczeœniej wyk³adnika specularExp, zrzutowana na wektor vec3.
+   vec3 specular;
+   if(intensity > 0.0)
+   {
+        intensity = max(0.0, dot(NN, NH));
+        specular = (vec3) pow(intensity, specularExp);
+   }
+   else
+        specular = intensity;
+   //g. Obliczenie koloru fragmentu (tylko pola .xyz) jako sumy sk³adowej œwiat³a rozproszonego (diffuse) i
+   //odbicia lustrzanego (specular).
+   gl_FragColor.xyz = diffuse + specular;
+   //h. Skopiowanie przezroczystoœci fragmentu z przezroczystoœci koloru interpolowanego
+   //gl_FragColor.a = gl_Color.a;
 }
--- L2 GLSL shaders Stud/shaders/specular.vs
+++ L2 GLSL shaders Stud/shaders/specular.vs
@@ -13,4 +13,18 @@ void main( void )
 
    // skopiowanie koloru wierzcho³ka (primary color)
    gl_FrontColor = gl_Color;
+
+   //Wyznaczenie wektora normalnego N w przestrzeni kamery jako iloczynu macierzy gl_NormalMatrix
+   //oraz wektora normalnego (gl_Normal). Wbudowana macierz gl_NormalMatrix jest odwrócon¹ i
+   //transponowan¹ lew¹ górn¹ podmacierz¹ 3x3 ModelView.
+   N = gl_NormalMatrix * gl_Normal;
+
+   //Obliczenie pomocniczego wektora vec4 V (wierzcho³ek w przestrzeni widoku modelu eye-space), jako
+   //iloczynu macierzy widoku modelu (gl_ModelViewMatrix) i wspó³rzêdnych wierzcho³ka
+   //(gl_Vertex).
+   vec4 V = gl_ModelViewMatrix * gl_Vertex;
+
+   //Wyznaczenie wektora œwiat³a L[ 0 ] (w przestrzenie widoku modelu eye-space) do Ÿród³a œwiat³a nr 0 jako
+   //ró¿nicy pomiêdzy pozycj¹ œwiat³a nr 0 (lightPos[ 0 ]) i wektorem V.xyz.
+   L[ 0 ] = lightPos[ 0 ] - V.xyz;
 }
